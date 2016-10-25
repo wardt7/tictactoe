646bd7d5e03e49e95e76e506c4cdcd2d22f88198
wardt7 <wardt7@uni.coventry.ac.uk>
2016-10-25 14:41:36 +0100

    Added a series of docstrings to functions in interface_cli.py. Also altered a few variable names to make them clear and shifted a section of code in the function main_menu to a new function as it was completing a different task to what the main_menu function's task is.

--- interface_cli.py
+++ interface_cli.py
@@ -1,4 +1,7 @@
 def main_menu():
+        """Starts the main menu of the game; provides a way of calling individual functions for
+        playing the game, and also handles some operations which don't require additional functions
+        such as exiting the entire game. Has no arguments and always returns None"""
         menu_on = True
         while menu_on:
                 print("Hello, welcome to the Tic Tac Toe game. What would you like to do?")
@@ -6,78 +9,85 @@ def main_menu():
                 print("B = Exit")
                 choice = input("Enter your choice")
                 if choice == "A":
-                        game_on = True
-                        board = [["-","-","-"],["-","-","-"],["-","-","-"]]
-                        player_one = True
-                        while game_on:
-                                print(board[0])
-                                print(board[1])
-                                print(board[2])
-                                if player_one:
-                                        print("Player one, make a move")
-                                else:
-                                        print("Player two, make a move")
-                                choice = input("Enter a number from 1 to 9")
-                                if choice == "1":
-                                        selection = board[2][0]
-                                elif choice == "2":
-                                        selection = board[2][1]
-                                elif choice == "3":
-                                        selection = board[2][2]
-                                elif choice == "4":
-                                        selection = board[1][0]
-                                elif choice == "5":
-                                        selection = board[1][1]
-                                elif choice == "6":
-                                        selection = board[1][2]
-                                elif choice == "7":
-                                        selection = board[0][0]
-                                elif choice == "8":
-                                        selection = board[0][1]
-                                elif choice == "9":
-                                        selection = board[0][2]
-                                else:
-                                        print("That was invalid")
-                                        continue
-                                if selection == "-":
-                                        pass
-                                else:
-                                        print("Invalid move, try again")
-                                        continue
-                                        
+                        local_game_2_player()                        
                 elif choice == "B":
                         print("Thanks for playing")
                         menu_on = False
                 else:
                         print("That was an invalid choice, try again")
         exit()
+
+def local_game_2_player():
+        """Starts a local 2 player game of tic tac toe. The function should keep running
+        until a winner is found or until 9 turns have elapsed (as after 9 turns there
+        are no more moves available). Has no arguments and always returns None."""
+        game_on = True
+        board = [["-","-","-"],["-","-","-"],["-","-","-"]]
+        player_one = True
+        while game_on:
+                print(board[0])
+                print(board[1])
+                print(board[2])
+                if player_one:
+                        print("Player one, make a move")
+                else:
+                        print("Player two, make a move")
+                choice = input("Enter a number from 1 to 9")
+                if choice == "1":
+                        selection = board[2][0]
+                elif choice == "2":
+                        selection = board[2][1]
+                elif choice == "3":
+                        selection = board[2][2]
+                elif choice == "4":
+                        selection = board[1][0]
+                elif choice == "5":
+                        selection = board[1][1]
+                elif choice == "6":
+                        selection = board[1][2]
+                elif choice == "7":
+                        selection = board[0][0]
+                elif choice == "8":
+                        selection = board[0][1]
+                elif choice == "9":
+                        selection = board[0][2]
+                else:
+                        print("That was invalid")
+                        continue
+                if selection == "-":
+                        pass
+                else:
+                        print("Invalid move, try again")
+                        continue
+
                         
 
 def win_check(board):
-        """Function to check if win condition reached"""
-        for i in range(3):
-            for j in range(3):
+        """Function to check if win condition reached. Input: board (board is a list containing 3 lists
+        each containing 3 elements. Each element is a "X", "O", or "-".). Output: "X", "O" (representing
+        the winner, Crosses or Noughts respectively), "-" (representing no winner)"""
+        for co_ord_y in range(3):
+            for co_ord_x in range(3):
                 # iterates through every section of the board
-                section = board[i][j]
+                section = board[co_ord_y][co_ord_x]
                 if section == "-":
                     # we don't need to check a section if it is blank
                     pass
                 else:
-                    for k in [-1, 0, 1]:
-                        for l in [-1, 0, 1]:
+                    for offset_y in [-1, 0, 1]:
+                        for offset_x in [-1, 0, 1]:
                             # iterate through all locations next to the location we previously found
-                            if i+k < 0 or j+l < 0:
+                            if co_ord_y+offset_y < 0 or co_ord_x+offset_x < 0:
                                 # We ignore negatives as these are outside the board
                                 pass
-                            elif k == 0 and l == 0:
+                            elif offset_y == 0 and offset_x == 0:
                                 # We create an exception to stop the use of the same position
                                 pass
                             else:
                                 try:
-                                    next_section = board[i+k][j+l]
+                                    next_section = board[co_ord_y+offset_y][co_ord_x+offset_x]
                                     if next_section == section:
-                                        if next_section == board[i+k+k][j+l+l]:
-                                            # Winner found; returns the symbol of the winner
+                                        if next_section == board[co_ord_y+offset_y+offset_y][co_ord_x+offset_x+offset_x]:
                                             return section
                                         else:
                                             # Two symbols were the same but the one after was not
@@ -86,7 +96,6 @@ def win_check(board):
                                         # The symbols are not the same so do not contribute to a complete line
                                         pass
                                 except IndexError:
-                                    # The index we passed was too large (looking for something outside the board
+                                    # The index we passed was too large (looking for something outside the board)
                                     pass
-        # No winner; dash returned
         return "-"
