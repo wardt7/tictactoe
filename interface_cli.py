f87c9720b1232be7319d18afdf11ba1e2a4acf5e
wardt7 <wardt7@uni.coventry.ac.uk>
2016-10-28 10:50:26 +0100

    Toby finished off the main local 2 player code and also fixed a bug within the win_check() function where negative indexes were permissible in certain circumstances. Other team member's code to follow.

--- interface_cli.py
+++ interface_cli.py
@@ -7,7 +7,7 @@ def main_menu():
                 print("Hello, welcome to the Tic Tac Toe game. What would you like to do?")
                 print("A = play a local game")
                 print("B = Exit")
-                choice = input("Enter your choice")
+                choice = input("Enter your choice\n")
                 if choice == "A":
                         local_game_2_player()                        
                 elif choice == "B":
@@ -24,6 +24,7 @@ def local_game_2_player():
         game_on = True
         board = [["-","-","-"],["-","-","-"],["-","-","-"]]
         player_one = True
+        turns = 0
         while game_on:
                 print(board[0])
                 print(board[1])
@@ -32,7 +33,8 @@ def local_game_2_player():
                         print("Player one, make a move")
                 else:
                         print("Player two, make a move")
-                choice = input("Enter a number from 1 to 9")
+                choice = input("Enter a number from 1 to 9\n")
+                # Obtains the symbol at the location selected to check for invalid moves
                 if choice == "1":
                         selection = board[2][0]
                 elif choice == "2":
@@ -55,7 +57,39 @@ def local_game_2_player():
                         print("That was invalid")
                         continue
                 if selection == "-":
-                        pass
+                        if player_one:
+                                symbol = "X"
+                        else:
+                                symbol = "O"
+                        # Changes the symbol at the selected location to the current player's token
+                        if choice == "1":
+                                board[2][0] = symbol
+                        elif choice == "2":
+                                board[2][1] = symbol
+                        elif choice == "3":
+                                board[2][2] = symbol
+                        elif choice == "4":
+                                board[1][0] = symbol
+                        elif choice == "5":
+                                board[1][1] = symbol
+                        elif choice == "6":
+                                board[1][2] = symbol
+                        elif choice == "7":
+                                board[0][0] = symbol
+                        elif choice == "8":
+                                board[0][1] = symbol
+                        else:
+                                board[0][2] = symbol
+                        win = win_check(board)
+                        if win != "-":
+                                return print(win, "is the winner! Congratulations!")
+                        else:
+                                turns += 1
+                                if turns == 9:
+                                        return print("It's a draw!")
+                                else:
+                                        player_one = not player_one
+                                        continue
                 else:
                         print("Invalid move, try again")
                         continue
@@ -87,7 +121,10 @@ def win_check(board):
                                 try:
                                     next_section = board[co_ord_y+offset_y][co_ord_x+offset_x]
                                     if next_section == section:
-                                        if next_section == board[co_ord_y+offset_y+offset_y][co_ord_x+offset_x+offset_x]:
+                                        if co_ord_y+offset_y+offset_y < 0 or co_ord_x+offset_x+offset_x < 0:
+                                                # We ignore negatives as these are outside the board
+                                                pass
+                                        elif section == board[co_ord_y+offset_y+offset_y][co_ord_x+offset_x+offset_x]:
                                             return section
                                         else:
                                             # Two symbols were the same but the one after was not
@@ -99,3 +136,5 @@ def win_check(board):
                                     # The index we passed was too large (looking for something outside the board)
                                     pass
         return "-"
+
+main_menu()
